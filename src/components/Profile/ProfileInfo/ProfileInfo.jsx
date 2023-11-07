4537fb4af9e9104d955c901f7d4cbb8392918799
MartaChobaniuk <marta.chobaniuk@gmail.com>
2023-11-07 13:39:01 +0200

    updated profileInfo

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -27,10 +27,44 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
         <br/>
         {isOwner && <input type={'file'} onChange={onMainPhotoSelected}/> }
 
+        <div>
+          <div>
+            <b>Full name:</b> { profile.fullName }
+          </div>
+
+          <div>
+            <b>Looking for a job:</b> { profile.lookingForAJob ? 'yes' : 'no'}
+          </div>
+
+          {profile.lookingForAJob &&
+            <div>
+              <b>My professional skills:</b> {profile.lookingForAJobDescription}
+            </div>
+          }
+
+          <div>
+            <b>About me:</b> { profile.aboutMe }
+          </div>
+
+          <div>
+            <b>Contacts:</b> { Object.keys(profile.contacts).map(key => {
+              return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
+            }) }
+          </div>
+        </div>
+
         <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
       </div>
     </div>
   );
+}
+
+const Contact = ({contactTitle, contactValue}) => {
+  return (
+    <div className={classes.contact}>
+      {contactTitle}: {contactValue}
+    </div>
+  )
 }
 
 export default ProfileInfo;
